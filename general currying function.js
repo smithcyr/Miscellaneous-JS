4e93048d46451c88f744fea10d77f1dd5d4e9101
smithcyr <smithcyr@grinnell.edu>
2013-04-03 22:25:57 -0500

    added specific functions for directions

--- general currying function.js
+++ general currying function.js
@@ -22,3 +22,37 @@ function directionUnaryRecurse(_adic_func,direction) {
 		}
 	return recurse;
 }
+function rightnaryRecurse(_adic_func) {
+	if (arguments.length == 0) {
+		return directionUnaryRecurse;
+	}
+	var num_arg = _adic_func.length,
+		args = Array(),
+		recurse = function (input) {
+			args.unshift(input);
+			if (args.length == num_arg) {
+				return _adic_func.apply(args);
+			}
+			else {
+				return recurse;
+			}
+		}
+	return recurse;
+}
+function leftUnaryRecurse(_adic_func) {
+	if (arguments.length == 0) {
+		return directionUnaryRecurse;
+	}
+	var num_arg = _adic_func.length,
+		args = Array(),
+		recurse = function (input) {
+			args.push(input);
+			if (args.length == num_arg) {
+				return _adic_func.apply(args);
+			}
+			else {
+				return recurse;
+			}
+		}
+	return recurse;
+}
